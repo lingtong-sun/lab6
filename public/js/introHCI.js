eee0592dbf21e2fb6cf254278950c5059fe14841
Lingtong Sun <lsun569@stanford.edu>
2014-02-12 16:32:16 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,21 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, displayProject);
+
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" +idNumber);
+}
+
+function displayProject(result){
+	console.log(result);
+
+	var detailHTML = "<img class='detailsImage' src = '" + result['image'] +"'>"
+				   + "<small>" + result['date'] + "</small>"
+				   + result['summary'];
+
+	$("#project" + result['id'] +" .details").html(detailHTML);
+	console.log("#project" + result['id'] +".details");
 }
 
 /*
@@ -35,4 +49,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", displayPalette);
+}
+
+function displayPalette(result) {
+	console.log(result);
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
